4dcb377578e28f6023b93a47e946e8e3cb972a40
scott306lr <scott306lr@gmail.com>
2024-08-26 01:50:30 +0800

    feat: :sparkles: Added new visualize graph, and removed seaborn dependency.

--- viewFormer/visualize/visualization.py
+++ viewFormer/visualize/visualization.py
@@ -1,6 +1,6 @@
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import math
 from ..utils.methods import absmax, absmax_reduce
 
@@ -13,11 +13,58 @@ def outlier_heatmap(data, kernel_size=1, axis=None, ax=None, *args, **kwargs):
     ax = ax or self_ax
 
     matrix = absmax_reduce(data, kernel_size=kernel_size, axis=axis)
-    g = sns.heatmap(matrix, ax=ax, **kwargs)
+    # g = sns.heatmap(matrix, ax=ax, **kwargs)
+    # matplot lib heatmap
+    g = ax.imshow(matrix, **kwargs)
 
     y_ticks = np.arange(0, matrix.shape[0], math.ceil(matrix.shape[0]/100)*10)
     x_ticks = np.arange(0, matrix.shape[1], math.ceil(matrix.shape[1]/100)*10)
 
     ax.set_yticks(y_ticks, labels=y_ticks*kernel_size)
     ax.set_xticks(x_ticks, labels=x_ticks*kernel_size)
+
+    # draw color bar, pad is the distance between color bar and plot
+    m = cm.ScalarMappable(**kwargs)
+    m.set_array(matrix)
+    ax.figure.colorbar(m, ax=ax, shrink=0.6, pad=0.1).ax.tick_params(labelsize=14)
+
+    # set axis font size
+    ax.tick_params(axis='both', which='major', labelsize=14)
+    
+    # set box aspect
+    ax.set_aspect('auto')
+
     return g
+
+
+def abs_outlier_tensor(w, ax=None, *args, **kwargs):
+    if ax is None:
+        fig, self_ax = plt.subplots(subplot_kw={'projection': '3d'}, figsize=(10, 10))
+    ax = ax or self_ax
+
+    # draw 3d surface plot with coolwarm color map
+    x = np.arange(w.shape[1])  # Dims
+    y = np.arange(w.shape[0])  # Seqlen
+    xpos, ypos = np.meshgrid(x, y, copy=False)
+    zpos = np.abs(w)
+    surf = ax.plot_surface(xpos, ypos, zpos, antialiased=False, **kwargs)
+
+    # set labels
+    ax.set_xlabel('Out Dims', fontsize=18, labelpad=10)
+    ax.set_ylabel('SeqLen', fontsize=18, labelpad=10)
+    ax.set_zlabel('Absolute Value', fontsize=18, labelpad=10)
+
+    # set axis font size
+    ax.tick_params(axis='both', which='major', labelsize=14)
+
+    # draw color bar, pad is the distance between color bar and plot
+    m = cm.ScalarMappable(**kwargs)
+    m.set_array(w)
+    ax.figure.colorbar(m, ax=ax, shrink=0.6, pad=0.1).ax.tick_params(labelsize=14)
+    
+
+    # set box aspect
+    # w_h = w.shape[1] / w.shape[0]
+    ax.set_box_aspect([1, 1, 1])
+
+    return surf
